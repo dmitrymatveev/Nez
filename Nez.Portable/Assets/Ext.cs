7a5af52872c53542a46d4660385ce16ac0268a68
dmitrymatveev <dmitrymatveevnz@gmail.com>
2019-11-13 20:35:55 +1300

    added extension to TitleContainer

---
+++ Nez.Portable/Assets/Ext.cs
@@ -0,0 +1,20 @@
+﻿#if EXTENDED
+using System;
+using System.IO;
+
+namespace Microsoft.Xna.Framework
+{
+    public static class TitleContainerExt
+    {
+        public static Func<string, Stream> OnGetStream;
+    }
+
+    public static class TitleContainer
+    {
+        public static Stream OpenStream(string name)
+        {
+            return TitleContainerExt.OnGetStream(name);
+        }
+    }
+}
+#endif
